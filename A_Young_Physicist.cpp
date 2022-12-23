14907fe80f00fbab9777184dce09f8cd1e7f691b
PankajAgrawal12 <104184475+PankajAgrawal12@users.noreply.github.com>
2022-12-23 19:37:01 +0530

    Create A_Young_Physicist.cpp

---
+++ A_Young_Physicist.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define w(t) ll t; cin>>t; while(t--)
+#define mod 1000000007
+
+#define B2(n) n, n + 1, n + 1, n + 2
+#define B4(n) B2(n), B2(n + 1), B2(n + 1), B2(n + 2)
+#define B6(n) B4(n), B4(n + 1), B4(n + 1), B4(n + 2)
+ll lookuptable[256] = { B6(0), B6(1), B6(1), B6(2) };
+ll countSetBits(ll N)
+{
+    ll count = lookuptable[N & 0xff] + lookuptable[(N >> 8) & 0xff] +lookuptable[(N >> 16) & 0xff] +lookuptable[(N >> 24) & 0xff];
+    return count;
+}
+
+using namespace std;
+
+int main()
+{
+	fast_io
+	int n;
+	cin>>n;
+	int sum1=0,sum2=0,sum3=0;
+	for(int i=0;i<n;i++)
+	{
+		
+			int x,y,z;
+			cin>>x>>y>>z;
+			sum1+=x;
+			sum2+=y;
+			sum3+=z;
+	}
+	if(sum1==0&&sum2==0&&sum3==0)
+		cout<<"YES\n";
+	else
+		cout<<"NO\n";
+	return 0;
+}
