ecf915587d9ed0ddc73be069d20073d3c3cc4a5a
PankajAgrawal12 <104184475+PankajAgrawal12@users.noreply.github.com>
2022-12-24 10:41:04 +0530

    Create A_Ilya_and_Bank_Account.cpp

---
+++ A_Ilya_and_Bank_Account.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define w(t) ll t; cin>>t; while(t--)
+#define mod 1000000000000000007
+
+#define B2(n) n, n + 1, n + 1, n + 2
+#define B4(n) B2(n), B2(n + 1), B2(n + 1), B2(n + 2)
+#define B6(n) B4(n), B4(n + 1), B4(n + 1), B4(n + 2)
+ll lookuptable[256] = { B6(0), B6(1), B6(1), B6(2) };
+ll countSetBits(ll N)
+{
+    ll count = lookuptable[N & 0xff] + lookuptable[(N >> 8) & 0xff] +lookuptable[(N >> 16) & 0xff] +lookuptable[(N >> 24) & 0xff];
+    return count;
+}
+
+using namespace std;
+
+int main()
+{
+	fast_io
+	ll n;
+	cin>>n;
+	if(n>0)
+		cout<<n<<endl;
+	else
+	{
+		ll rem=n%10;
+		ll ans1=n/10;
+		ll ans2=n/100;
+		ans2=(ans2*10)+rem;
+		cout<<max(ans1,ans2);
+	}
+	return 0;
+}
+
