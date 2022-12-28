c3621aef70e316adbb787dc93a21fc0d88812016
PankajAgrawal12 <104184475+PankajAgrawal12@users.noreply.github.com>
2022-12-28 11:40:52 +0530

    Create A_Joey_Takes_Money.cpp

---
+++ A_Joey_Takes_Money.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define w(t) ll t; cin>>t; while(t--)
+#define mod 1000000000000000007
+
+#define B2(n) n, n + 1, n + 1, n + 2
+#define B4(n) B2(n), B2(n + 1), B2(n + 1), B2(n + 2)
+#define B6(n) B4(n), B4(n + 1), B4(n + 1), B4(n + 2)
+ll lookuptable[256] = { B6(0), B6(1), B6(1), B6(2) };
+ll countSetBits(ll N)
+{
+    ll count = lookuptable[N & 0xff] + lookuptable[(N >> 8) & 0xff] +lookuptable[(N >> 16) & 0xff] +lookuptable[(N >> 24) & 0xff];
+    return count;
+}
+
+using namespace std;
+
+int main()
+{
+	fast_io
+	w(t)
+	{
+		ll n;
+		cin>>n;
+		ll arr[n];
+		ll pro=1;
+		for(ll i=0;i<n;i++)
+		{
+			cin>>arr[i];
+			pro=(pro*arr[i])%mod;
+		}
+			
+		
+		ll sum=pro+n-1;
+		
+		cout<<(sum*2022)%mod<<endl;;
+	}
+	
+	return 0;
+	
+}
