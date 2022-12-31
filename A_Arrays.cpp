7a6c0be75f512514f9df37cafaef6b37abd37f62
PankajAgrawal12 <104184475+PankajAgrawal12@users.noreply.github.com>
2022-12-31 09:57:41 +0530

    Create A_Arrays.cpp

---
+++ A_Arrays.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define w(t) ll t; cin>>t; while(t--)
+#define mod 1000000007
+using namespace std;
+int main()
+{
+	fast_io
+int n; scanf("%d", &n);
+    std::vector<int> first, second, third;
+    int temp(0);
+    for(int k = 0; k < n; k++){
+        scanf("%d", &temp);
+        if(temp < 0){first.push_back(temp);}
+        else if(temp == 0){second.push_back(temp);}
+        else if(temp > 0){third.push_back(temp);}
+    }
+    if(third.size() == 0){
+        temp = first.back();first.pop_back();third.push_back(temp);
+        temp = first.back();first.pop_back();third.push_back(temp);
+    }
+    if(first.size() % 2 == 0){temp = first.back(); first.pop_back(); second.push_back(temp);}
+    
+    printf("%lu ", first.size()); for(int k = 0; k < first.size();  k++){printf("%d ", first[k]);};  printf("\n");
+    printf("%lu ", third.size()); for(int k = 0; k < third.size();  k++){printf("%d ", third[k]);};  printf("\n");
+    printf("%lu ", second.size());for(int k = 0; k < second.size(); k++){printf("%d ", second[k]);}; printf("\n");
+	return 0;
+}
