4788a1d03d4c3dfcfeefd6f936cf977007ee0a65
PankajAgrawal12 <104184475+PankajAgrawal12@users.noreply.github.com>
2023-01-05 21:37:23 +0530

    Create A_Greatest_Convex.cpp

---
+++ A_Greatest_Convex.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define w(t) ll t; cin>>t; while(t--)
+#define mod 1000000007
+using namespace std;
+ 
+int main()
+{
+	fast_io
+	w(t)
+	{
+	   ll k;
+	   cin>>k;
+	   cout<<k-1<<endl;
+	    
+	}
+	return 0;
+}
