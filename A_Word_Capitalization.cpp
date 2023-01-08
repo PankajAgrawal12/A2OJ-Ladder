a72a488a2574905d40e1add2401a1be46e7fcc2c
PankajAgrawal12 <104184475+PankajAgrawal12@users.noreply.github.com>
2023-01-08 09:59:29 +0530

    Create A_Word_Capitalization.cpp

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define w(t) ll t; cin>>t; while(t--)
+#define mod 1000000007
+using namespace std;
+
+int main()
+{
+	fast_io
+	string s;
+	cin>>s;
+	s[0]=toupper(s[0]);
+	cout<<s<<endl;
+
+	return 0;
+}
